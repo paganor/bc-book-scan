f85bca8f1e2b03de1a29419cce0f86f3f05bc62d
Rob Pagano <rob@bigcompass.com>
2015-05-06 11:16:29 -0600

    added avatar img for scans
        * reads from Google
        * uses hyperlink for now, might want to look into caching

--- app/scripts/controllers/barcode.js
+++ app/scripts/controllers/barcode.js
@@ -2,30 +2,14 @@
 
 app.controller('BarcodeCtrl', function($scope,
                                        $ionicPlatform,
-                                       $cordovaToast,
                                        $cordovaBarcodeScanner) {
-  $scope.log = function() {
-    $ionicPlatform.ready(function() {
-      $cordovaToast
-        .showLongCenter('heyyyy..')
-        .then(function(success) {
-          console.log('was success', success);
-        }, function(error) {
-          console.log('was error', error);
-        });
-    });
-  };
 
   $scope.launchScanner = function() {
     $ionicPlatform.ready(function() {
       $cordovaBarcodeScanner
         .scan()
         .then(function(barcodeData) {
-          $cordovaToast
-            .showLongCenter(barcodeData)
-            .then(function(success) {
-              console.log(success);
-            });
+          console.log(barcodeData);
         }, function(error) {
           // An error occurred
           console.log(error);
