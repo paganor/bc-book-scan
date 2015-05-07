20f0acb08120b505c941a7811113e93cff0d216a
Rob Pagano <rob@bigcompass.com>
2015-05-07 11:37:44 -0600

    currency, scan view, isbn validation, random price
        * added filters for isbn
        * added very brief detail section for scan
        * small image for scan (book)

--- app/scripts/services/scan.js
+++ app/scripts/services/scan.js
@@ -30,20 +30,44 @@ app.factory('scan', function($q, $http, uuid4) {
 
     var newBook = {};
 
-    $http.get('http://thor.bigcompass.com:5555/rest/bookScan/book/' + value)
-      .success(function(data) {
-        // success function
-        newBook = data.items[0];
-        newBook.scanTime = new Date().getTime();
-        newBook.uuid = uuid4.generate();
-        _allScans.splice(0, 0, newBook);
-        deferred.resolve(newBook);
-      })
-      .error(function(data, status) {
-        // failure callback
-        console.log(data, status);
-        deferred.reject('lookup-error');
-      });
+    // validate scan is a book
+    var isbn;
+
+    switch(type) {
+      case 'EAN_13':
+        isbn = value;
+        break;
+      case 'EAN_10':
+        isbn = '978' + value;
+        break;
+      default:
+        isbn = null;
+        deferred.reject('not-a-book');
+        break;
+    }
+
+    if(isbn) {
+      $http.get('http://thor.bigcompass.com:5555/rest/bookScan/book/' + isbn)
+        .success(function(data) {
+          // success function
+          if(data.totalItems === 0) {
+            deferred.reject('book-not-found');
+          } else {
+            newBook = data.items[0];
+            newBook.scanTime = new Date().getTime();
+            newBook.uuid = uuid4.generate();
+            newBook.price = Math.random() * 100;
+            _allScans.splice(0, 0, newBook);
+
+            deferred.resolve(newBook);
+          }
+        })
+        .error(function(data, status) {
+          // failure callback
+          console.log(data, status);
+          deferred.reject('lookup-error');
+        });
+    }
 
       return deferred.promise;
   };
